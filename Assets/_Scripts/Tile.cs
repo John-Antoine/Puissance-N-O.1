94bf9b3a4f2f8d079f9dcfa5bac3f12ee03652b6
Antoine Lenormand <antoine.lenormand@steady-sun.com>
2022-01-27 06:20:32 +0100

    tentative

---
+++ Assets/_Scripts/Tile.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Windows;
+
+
+
+public class Tile : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _highlight;
+    [SerializeField] private GameObject _cliquayP1, _cliquayP2;
+    public int WhosTurn = 1;
+
+    
+    void OnMouseEnter()
+    {
+        _highlight.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        _highlight.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+        if (WhosTurn == 1)
+        {
+            _cliquayP1.SetActive(true);
+            _cliquayP2.SetActive(false);
+        }
+        else if (WhosTurn == 2)
+        {
+            _cliquayP2.SetActive(true);
+            _cliquayP1.SetActive(false);
+        }
+
+
+        ChangePlayer();
+    }
+
+    void ChangePlayer()
+    {
+        if (WhosTurn == 1)
+        {
+            WhosTurn = 2;
+        }
+        else if (WhosTurn == 2)
+        {
+            WhosTurn = 1;
+        }
+    }
+}
---
+++ Assets/_Scripts/TransferDimensions.cs
@@ -0,0 +1,26 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TransferDimensions : MonoBehaviour
+{
+    public int _width;
+    public int _height;
+    public int _winLine; 
+    public GameObject InputL;
+    public GameObject InputH;
+    public GameObject InputNb;
+
+
+    public StoreInfos()
+    {
+        _width = int.Parse(InputL.GetComponent<Text>().text);
+        _height = int.Parse(InputH.GetComponent<Text>().text);
+        _winLine = int.Parse(InputNb.GetComponent<Text>().text);
+    }
+
+    return _width; 
+    return _height;
+    return _winLine;
+}*/
